26a93074f6b30234db7070cbeb9986f4c3c18329
Roberto C. Hernandez <carlosh95@gmail.com>
2016-03-25 11:36:08 -0400

    index fixed

--- src/index.js
+++ src/index.js
@@ -1,46 +1,34 @@
 'use strict';
 
-<<<<<<< HEAD
-var plugins = require('gulp-load-plugins')({ lazy: true });
-=======
 var eslint = require('gulp-eslint');
->>>>>>> 9f61d5728960b436cfe33bb3f21ea79f579d7967
 var fs = require('fs');
 var handyman = require('pipeline-handyman');
 var path = require('path');
 var lazypipe = require('lazypipe');
-<<<<<<< HEAD
-=======
 
->>>>>>> 9f61d5728960b436cfe33bb3f21ea79f579d7967
 var esLintConfig = resolveConfigFile('.eslintrc');
 
 module.exports = {
   validateJS: function (options) {
-<<<<<<< HEAD
-    var defaultConfig = JSON.parse(fs.readFileSync(esLintConfig, 'utf8'));
-    var optionType = typeof options;
-    var customConfig, config, rules;
-=======
     var dest = JSON.parse(fs.readFileSync(esLintConfig, 'utf8'));
     var customConfig = {};
     var origin = {};
-    var rules = {};
->>>>>>> 9f61d5728960b436cfe33bb3f21ea79f579d7967
 
-    if (options) {
-      if (optionType === 'object' && !Array.isArray(options)) {
-        esLintConfig = handyman.mergeConfig(defaultConfig, options);
-      } else if (optionType === 'string') {
-        customConfig = resolveConfigFile(options);
-        config = JSON.parse(fs.readFileSync(customConfig, 'utf8'));
-
-        esLintConfig = handyman.mergeConfig(defaultConfig, config);
+    if (options && typeof options === 'object' && !Array.isArray(options)
+       || options && typeof options === 'string') {
+      if (typeof options === 'object') {
+        esLintConfig = handyman.mergeConfig(dest, options);
       } else {
-        handyman.log('Validading js with ESlint ecmaScript5, ** Options not valid **');
+        customConfig = resolveConfigFile(options);
+        origin = JSON.parse(fs.readFileSync(customConfig, 'utf8'));
+
+        esLintConfig = handyman.mergeConfig(dest, origin);
       }
+    } else if (options) {
+      handyman.log('** Options not valid **');
     }
 
+    handyman.log('Validading js with ESlint');
     return validateES();
   }
 };
@@ -77,6 +65,5 @@ function validateES() {
     .pipe(eslint.format)
     .pipe(eslint.failOnError);
 
-  handyman.log('Validading js with ESlint ecmaScript5');
   return stream();
 }
