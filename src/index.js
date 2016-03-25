521dcec72fb3679c4b2845957cbe45afeca38c5b
Roberto C. Hernandez <carlosh95@gmail.com>
2016-03-25 15:39:50 -0400

    refactored pack.json, index.js, and fixed vars option on lintrc

--- src/index.js
+++ src/index.js
@@ -10,28 +10,32 @@ var esLintConfig = resolveConfigFile('.eslintrc');
 
 module.exports = {
   validateJS: function (options) {
-    var dest = JSON.parse(fs.readFileSync(esLintConfig, 'utf8'));
-    var customConfig = {};
-    var origin = {};
-
-    if ((options && typeof options === 'object' && !Array.isArray(options))
-       || (options && typeof options === 'string')) {
-      if (typeof options === 'object') {
-        esLintConfig = handyman.mergeConfig(dest, options);
-      } else {
-        customConfig = resolveConfigFile(options);
-        origin = JSON.parse(fs.readFileSync(customConfig, 'utf8'));
-
-        esLintConfig = handyman.mergeConfig(dest, origin);
-      }
-    } else if (options) {
-      handyman.log('** Options not valid **');
-    }
-
+    checkOptions(options);
     handyman.log('Validading js with ESlint');
+
     return validateES();
   }
 };
+
+function checkOptions(options) {
+  var dest = JSON.parse(fs.readFileSync(esLintConfig, 'utf8'));
+  var customConfig = {};
+  var origin = {};
+
+  if ((options && typeof options === 'object' && !Array.isArray(options))
+    || (options && typeof options === 'string')) {
+    if (typeof options === 'object') {
+      esLintConfig = handyman.mergeConfig(dest, options);
+    } else {
+      customConfig = resolveConfigFile(options);
+      origin = JSON.parse(fs.readFileSync(customConfig, 'utf8'));
+
+      esLintConfig = handyman.mergeConfig(dest, origin);
+    }
+  } else if (options) {
+    handyman.log('** Options not valid **');
+  }
+}
 
 function resolveConfigFile(fileName) {
   var configFilesPathUser = path.resolve(process.cwd(), fileName);
