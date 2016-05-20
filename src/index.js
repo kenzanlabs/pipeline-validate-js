168e9740abc8a0b566fbe0f2559b1f21d777baa0
Paul Barry <pbarry@kenzan.com>
2016-05-20 16:49:21 -0400

    Initial stages of refactor

--- src/index.js
+++ src/index.js
@@ -8,89 +8,109 @@ var path = require('path');
 var _ = require('lodash');
 
 var ESLINT_CONFIG_PATH = './.eslintrc';
-var esLintConfig = resolveConfigFile(ESLINT_CONFIG_PATH);
+//var esLintConfig = resolveConfigFile(ESLINT_CONFIG_PATH);
 
 module.exports = {
-  validateJS: function (options) {
-    if (options) { checkOptions(options); }
-    checkLocalLintFile();
 
-    handyman.log('Validading js with ESlint');
-    return pipelineFactory();
+  validateJS: function (options) {
+    //  if (options) {
+    //    checkOptions(options);
+    //  }
+    //  checkLocalLintFile();
+    //
+    //  return pipelineFactory();
   }
+
 };
 
-function checkLocalLintFile() {
-  var rootFile = process.cwd() + '/.eslintrc';
-  var file = './node_modules/pipeline-validate-js/.eslintrc';
-
-  fs.readFile(rootFile, function (err) {
-    if (err) {return;}
-
-    fs.readFile(file, 'utf8', function (err, data) {
-      if (err) { return; }
-
-      handyman.log('merging local and custom .eslintrc file');
-      esLintConfig = handyman.mergeConfig(esLintConfig, data);
-    });
-  });
-}
-
-function checkOptions(options) {
-  var dest = JSON.parse(fs.readFileSync(esLintConfig, 'utf8'));
-  var customConfig = {};
-  var origin = {};
-
-  if (_.isPlainObject(options)) {
-    handyman.log('Parsing Options');
-    esLintConfig = handyman.mergeConfig(dest, options);
-
-  } else if (typeof options === 'string') {
-    handyman.log('Linting using custom file');
-
-    customConfig = resolveConfigFile(options);
-    origin = JSON.parse(fs.readFileSync(customConfig, 'utf8'));
-    esLintConfig = handyman.mergeConfig(dest, origin);
-  } else {
-    handyman.log('** Options not valid **');
-
-    throw new ReferenceError();
-  }
-}
-
-function resolveConfigFile(fileName) {
-  var configFilesPathUser = path.resolve(process.cwd(), fileName);
-  var configFilesPathDefault = __dirname.substring(0, __dirname.lastIndexOf('/'));
-
-  configFilesPathDefault = path.resolve(configFilesPathDefault, fileName);
-
-  return existsSync(configFilesPathUser) ? configFilesPathUser : configFilesPathDefault;
-}
-
-function existsSync(filename) {
-  if (typeof fs.accessSync === 'function') {
-    try {
-      fs.accessSync(filename);
-      handyman.log('Linting using ' + filename);
-      return true;
-    } catch (error) {
-      if (typeof error !== 'object' || error.code !== 'ENOENT') {
-        handyman.log('Unable to access ' + filename + ':');
-        handyman.log(error.stack);
-      }
-      return false;
-    }
-  } else {
-    return fs.existsSync(filename);
-  }
-}
-
-function pipelineFactory() {
-  var stream = lazypipe()
-    .pipe(eslint, esLintConfig)
-    .pipe(eslint.format)
-    .pipe(eslint.failOnError);
-
-  esLintConfig = resolveConfigFile(ESLINT_CONFIG_PATH);
-  return stream();
-}
+//function checkLocalLintFile () {
+//  var repoFilePath = process.cwd() + '/.eslintrc';
+//  var libraryFilePath = './node_modules/pipeline-validate-js/.eslintrc';
+//  var repoFile;
+//  var libraryFile;
+//
+//  console.log('cwd', process.cwd());
+//  console.log('cwd', process.cwd());
+//
+//  fs.readFile(repoFilePath, 'utf-8', function (err, repoFileData) {
+//    if (err) {
+//      return;
+//    }
+//
+//    handyman.log('Repo .eslintrc available, merging with default');
+//    repoFile = repoFileData;
+//
+//    // access default from package.
+//    fs.access(libraryFilePath, fs.F_OK, function (err) {
+//      console.log(err ? 'no access!' : 'can read/write');
+//    });
+//    //
+//    //fs.readFile(libraryFilePath, 'utf8', function (err, data) {
+//    //  if (err) {
+//    //    return;
+//    //  }
+//    //
+//    //  handyman.log('merging local and custom .eslintrc file');
+//    //  esLintConfig = handyman.mergeConfig(esLintConfig, data);
+//    //});
+//  });
+//}
+//
+//function checkOptions (options) {
+//  var dest = JSON.parse(fs.readFileSync(esLintConfig, 'utf8'));
+//  var customConfig = {};
+//  var origin = {};
+//
+//  if (_.isPlainObject(options)) {
+//    handyman.log('Parsing Options');
+//    esLintConfig = handyman.mergeConfig(dest, options);
+//
+//  } else if (typeof options === 'string') {
+//    handyman.log('Linting using custom file');
+//
+//    customConfig = resolveConfigFile(options);
+//    origin = JSON.parse(fs.readFileSync(customConfig, 'utf8'));
+//    esLintConfig = handyman.mergeConfig(dest, origin);
+//  } else {
+//    handyman.log('** Options not valid **');
+//
+//    throw new ReferenceError();
+//  }
+//}
+//
+//function resolveConfigFile (fileName) {
+//  var configFilesPathUser = path.resolve(process.cwd(), fileName);
+//  var configFilesPathDefault = __dirname.substring(0, __dirname.lastIndexOf('/'));
+//
+//  configFilesPathDefault = path.resolve(configFilesPathDefault, fileName);
+//
+//  return existsSync(configFilesPathUser) ? configFilesPathUser : configFilesPathDefault;
+//}
+//
+//function existsSync (filename) {
+//  if (typeof fs.accessSync === 'function') {
+//    try {
+//      fs.accessSync(filename);
+//      handyman.log('Linting using ' + filename);
+//      return true;
+//    } catch (error) {
+//      if (typeof error !== 'object' || error.code !== 'ENOENT') {
+//        handyman.log('Unable to access ' + filename + ':');
+//        handyman.log(error.stack);
+//      }
+//      return false;
+//    }
+//  } else {
+//    return fs.existsSync(filename);
+//  }
+//}
+//
+//function pipelineFactory () {
+//  var stream = lazypipe()
+//    .pipe(eslint, esLintConfig)
+//    .pipe(eslint.format)
+//    .pipe(eslint.failOnError);
+//
+//  esLintConfig = resolveConfigFile(ESLINT_CONFIG_PATH);
+//  return stream();
+//}
--- test/index.spec.js
+++ test/index.spec.js
@@ -9,101 +9,127 @@ var validatePipeline = require('../src/index.js');
 chai.should();
 chai.use(sinonChai);
 
-describe('pipeline-validateJS', function() {
-  var pipeline;
+describe('pipeline-validateJS', function () {
 
-  beforeEach(function() {
-    pipeline = validatePipeline.validateJS;
-  });
+  describe('Initialization', function () {
 
-  it('should return a object', function () {
-    (typeof validatePipeline).should.equal('object');
-  });
-
-  it('should contain a validateJS method', function() {
-    pipeline = validatePipeline.validateJS;
-    pipeline.should.exist;
-    (typeof pipeline).should.equal('function');
-  });
-
-  describe('validateJS method', function () {
-    var stream;
-
-    beforeEach(function() {
-      stream = function() {return pipeline();};
+    it('should exist as an object', function () {
+      (validatePipeline).should.exist;
+      (typeof validatePipeline).should.equal('object');
     });
 
-    it('should return a stream', function() {
+    it('should expose a validateJS method', function () {
+      (validatePipeline.validateJS).should.exist;
+      (typeof validatePipeline.validateJS).should.equal('function');
+    });
+
+  });
+
+  xdescribe('validateJS method', function () {
+    var validateJS;
+
+    beforeEach(function () {
+      validateJS = validatePipeline.validateJS;
+    });
+
+    it('should return a stream', function () {
       isStream(stream()).should.equal(true);
     });
 
-    describe('validateJS pipeline with no options', function() {
+  });
+
+  /*
+   should log a message stating that the default (included) .eslintrc is being used
+   should log a message stating that a repo .eslintrc is available, and being merged
+   should check that the merged rules
+   */
+
+  xdescribe('validateJS method', function () {
+    var stream;
+
+    beforeEach(function () {
+      stream = function () {
+        return pipeline();
+      };
+    });
+
+    it('should return a stream', function () {
+      isStream(stream()).should.equal(true);
+    });
+
+    describe('validateJS pipeline with no options', function () {
       var sandbox = {};
       var spy = {};
 
-      beforeEach(function() {
+      beforeEach(function () {
         sandbox = sinon.sandbox.create();
         spy = sandbox.spy(handyman, 'log');
       });
 
-      afterEach(function() {
+      afterEach(function () {
         sandbox.restore();
       });
 
-      it('should test validateJS() with no options', function() {
+      it('should test validateJS() with no options', function () {
         pipeline();
         spy.should.have.been.calledWith('Validading js with ESlint');
       });
     });
 
-    describe('ValidateJS Pipeline with options', function() {
+    describe('ValidateJS Pipeline with options', function () {
       var sandbox = {};
       var spy = {};
       var fn;
 
-      beforeEach(function() {
+      beforeEach(function () {
         sandbox = sinon.sandbox.create();
         spy = sandbox.spy(handyman, 'log');
       });
 
-      afterEach(function() {
+      afterEach(function () {
         sandbox.restore();
       });
 
-      it('should test validateJS() with invalid options, number', function() {
-        fn = function() {pipeline(234);};
+      it('should test validateJS() with invalid options, number', function () {
+        fn = function () {
+          pipeline(234);
+        };
 
         fn.should.throw();
         spy.should.have.been.calledWith('** Options not valid **');
       });
 
-      it('should test validateJS() with invalid options, array', function() {
-        fn = function() {pipeline(['semi', 1]);};
+      it('should test validateJS() with invalid options, array', function () {
+        fn = function () {
+          pipeline(['semi', 1]);
+        };
 
         fn.should.throw();
         spy.should.have.been.calledWith('** Options not valid **');
       });
 
-      it('should test validateJS() with an invalid file path as an  option', function() {
-        fn = function() { pipeline('.eslintrc1'); };
+      it('should test validateJS() with an invalid file path as an  option', function () {
+        fn = function () {
+          pipeline('.eslintrc1');
+        };
 
         fn.should.throw();
       });
 
-      it('should test validateJS() with valid url as options', function() {
+      it('should test validateJS() with valid url as options', function () {
         pipeline('./test/fixtures/.eslintrc3');
 
         spy.should.have.been.calledWith('Linting using custom file');
       });
 
-      it('should test validateJS() with valid url as options', function() {
+      it('should test validateJS() with valid url as options', function () {
         pipeline('./test/fixtures/.eslintrc3');
 
         spy.should.have.been.calledWith(sinon.match(/^Linting using.*eslintrc3$/));
       });
 
-      it('should test validateJS() with valid object as options', function() {
-        pipeline({ 'rules': { 'semi': 2 }});
+      it('should test validateJS() with valid object as options', function () {
+        pipeline({'rules': {'semi': 2}});
 
         spy.should.have.been.calledWith('Parsing Options');
       });
