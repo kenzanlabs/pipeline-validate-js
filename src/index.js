0a734f7aa1c3304c1daeacdcbda3cc226cdadba2
Matthew Gardner <mgardner@kenzanmedia.com>
2016-06-29 16:43:21 -0400

    Changed the code to match the original design and removed unncessary functions

--- src/index.js
+++ src/index.js
@@ -11,76 +11,75 @@ var NODE_MODULES_PATH = 'node_modules/pipeline-validate-js/.eslintrc';
 
 module.exports = {
   validateJS: function (options) {
-    var config = module.exports.getLintConfig(options);
+    var config = checkLocalLintFile(options);
 
     handyman.log('Validading js with ESlint');
-    return module.exports.validate(config);
-  },
+    return pipelineFactory(config);
+  }
+};
 
-  getLintConfig: function (options) {
-    var config = {};
-    var defaultPath = path.join(process.cwd(), NODE_MODULES_PATH);
-    var rootPath = path.join(process.cwd(), '.eslintrc');
-    var customConfig;
+function checkLocalLintFile(options) {
+  var config = {};
+  var defaultPath = path.join(process.cwd(), NODE_MODULES_PATH);
+  var rootPath = path.join(process.cwd(), '.eslintrc');
+  var customConfig;
+
+  try {
+    handyman.log('Linting using default path: ' + defaultPath);
+    config = JSON.parse(fs.readFileSync(defaultPath, 'utf-8'));
+
+  } catch (ex) {
+    // no op.
+
+  }
+
+  if (options) {
+
+    if (typeof options === 'string') {
+      customConfig = fs.readFileSync(options, 'utf-8');
+      handyman.log('Linting using custom path: ' + options);
+      config = handyman.mergeConfig(config, JSON.parse(customConfig));
+      handyman.log('Linting using custom file');
+
+    } else if (_.isPlainObject(options)) {
+      handyman.log('Parsing Options');
+      config = handyman.mergeConfig(config, options);
+
+    } else {
+      handyman.log('** Options not valid **');
+      throw new ReferenceError();
+
+    }
+  } else {
 
     try {
-      handyman.log('Linting using default path: ' + defaultPath);
-      config = JSON.parse(fs.readFileSync(defaultPath, 'utf-8'));
+      customConfig = fs.readFileSync(rootPath, 'utf-8');
+      handyman.log('Linting using root path: ' + rootPath);
+      config = handyman.mergeConfig(config, customConfig);
 
     } catch (ex) {
       // no op.
-
     }
 
-    if (options) {
+  }
 
-      if (typeof options === 'string') {
-        customConfig = fs.readFileSync(options, 'utf-8');
-        handyman.log('Linting using custom path: ' + options);
-        config = handyman.mergeConfig(config, JSON.parse(customConfig));
-        handyman.log('Linting using custom file');
+  return config;
+}
 
-      } else if (_.isPlainObject(options)) {
-        handyman.log('Parsing Options');
-        config = handyman.mergeConfig(config, options);
+function pipelineFactory(config) {
+  var stream;
 
-      } else {
-        handyman.log('** Options not valid **');
-        throw new ReferenceError();
+  if (typeof config === 'undefined') {
+    handyman.log('Validate error! Config object required');
+    return false;
 
-      }
-    } else {
-
-      try {
-        customConfig = fs.readFileSync(rootPath, 'utf-8');
-        handyman.log('Linting using root path: ' + rootPath);
-        config = handyman.mergeConfig(config, customConfig);
-
-      } catch (ex) {
-        // no op.
-      }
-
-    }
-
-    return config;
-  },
-
-  validate: function (config) {
-    var stream;
-
-    if (typeof config === 'undefined') {
-      handyman.log('Validate error! Config object required');
-      return false;
-
-    } else {
-      stream = lazypipe()
+  } else {
+    stream = lazypipe()
         .pipe(eslint.format)
         .pipe(eslint, config)
         .pipe(eslint.failOnError);
 
-      return stream();
-
-    }
+    return stream();
 
   }
-};
+}
